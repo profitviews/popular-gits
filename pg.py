ba1d4adf0328bd60d2f40188ad314b03752d828d
Richard Hickling <richard.t.hickling@gmail.com>
2022-07-22 16:30:26 +0200

    Refining log level specification

--- pg.py
+++ pg.py
@@ -13,12 +13,14 @@ parser.add_argument('db_name', help="Root name of SQLite database to be created 
 parser.add_argument('org', help="Github organization name")
 parser.add_argument('repo', help="Github repo name, i.e. github.com/org/repo")
 parser.add_argument('--reset', '-r', help="Erase the database if it exists", action="store_true")
-parser.add_argument('--log', '-l', help=f"Specify the log level.  Default is {logging.getLogger().__dict__['level']}")
+parser.add_argument('--log', '-l', choices=logging._nameToLevel.keys() , help=f"Specify the log level.  Default is {logging._levelToName[logging.getLogger().__dict__['level']]}")
 
 if __name__ == "__main__":
     args = parser.parse_args()
     logger = logging.getLogger()
-    logger.setLevel(int(args.log) if args.log else logging.INFO)
+
+    if args.log:
+        logger.setLevel(args.log)
     logger.info("Starting popular_gits")
     pg = popular_gits.popular_gits(args.db_name, os.environ['GITHUB_KEY'], args.org, args.repo, logger)
     if args.reset:
