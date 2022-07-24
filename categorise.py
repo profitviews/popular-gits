c8bd794d727b7c865b50756e91e1e3ddcea879fe
Richard Hickling <richard.t.hickling@gmail.com>
2022-07-24 21:21:52 +0200

    Code to add relative popularity (uses GraphQL)

--- categorise.py
+++ categorise.py
@@ -1,8 +1,12 @@
-
+import os
 import csv
+import asyncio
+import nest_asyncio
 from itertools import combinations
 from collections import defaultdict
-
+from github import Github
+from gql import gql, Client
+from gql.transport.aiohttp import AIOHTTPTransport
 
 def get_popularity_lists(categorized_popularity_file):
     """ From a CSV file created from popular_gits.py's 'gits' database table
@@ -15,14 +19,14 @@ def get_popularity_lists(categorized_popularity_file):
     Args:
     categorized_popularity_file: the CSV file with structure as above
     """
-    pop_lists = defaultdict(list)
+    popularity_lists = defaultdict(list)
     with open(categorized_popularity_file) as csvfile:
         for git in csv.DictReader(csvfile):
             c = git['Categories'].split(',')
             for l in range(1,len(c)):
                 for s in combinations(c, l):
-                    pop_lists[tuple(sorted(s))].append((git['URL'],git['Popularity']))
-    return pop_lists
+                    popularity_lists[tuple(sorted(s))].append((git['URL'],git['Popularity']))
+    return popularity_lists
 
 
 def create_popularity_list_file(popularity_lists, popularity_list_file):
@@ -45,3 +49,41 @@ def create_popularity_list_file(popularity_lists, popularity_list_file):
             r = sorted(r, key=lambda rep: rep[1], reverse=True)
             for repo,_ in r:
                 pl.writerow([cts,repo])
+
+
+def get_relative_popularity(popularity_file, output_file):
+    """ From a CSV file with GitHub URL and Popularity, get the overall stars for the URL
+    and calculate the relative popularity.
+    Create a new CSV with relative popularity and overall stars
+
+    Args:
+        popularity_file: must have columns URL and Popularity
+        output_file: add columns OverallStars and RelativePopularity
+    """
+
+    nest_asyncio.apply()
+    # We need the GraphQL API to get the count of Stargazers when over 40K
+    transport = AIOHTTPTransport(
+        url="https://api.github.com/graphql", 
+        headers={'Authorization': f'token {os.environ["GITQL_KEY"]}'})
+    client = Client(transport=transport, fetch_schema_from_transport=True)
+
+    async def execute_query(client, query):  # So it works in Jupyter and other async contexts
+        result = await client.execute_async(query)
+        return result
+    
+    with open(popularity_file) as pop_file, open(output_file, 'w') as rel_pop:
+        gits = csv.DictReader(pop_file)
+        r = csv.writer(rel_pop)
+        r.writerow(gits.fieldnames + ['OverallStars', 'RelativePopularity'])
+        g = Github(os.environ['GITHUB_KEY'])
+        for git in gits:
+            repo_id = git['URL'].split('.com/')[1]
+            repo = g.get_repo(repo_id)
+            owner,name = repo_id.split('/')
+            if (rc := repo.get_stargazers().totalCount) >= 40_000:  # Magic max cut-off
+                query = gql(f'query {{ repository(owner:"{owner}", name:"{name}") {{stargazerCount}} }}')
+                # See execute_query: lets it work in Jupyter and other async contexts
+                rc = dict(asyncio.get_event_loop().run_until_complete(execute_query(client, query)))['repository']['stargazerCount']
+                print(f"Repo count: {rc}")
+            r.writerow(list(git.values()) + [rc, int(git['Popularity'])/rc])
